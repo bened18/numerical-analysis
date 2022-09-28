2c266e39dfff8452ced1d944723e69209b6b17c6
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-09-28 11:01:13 -0500

    Added table to bisection

--- methods/python-methods/bisection.py
+++ methods/python-methods/bisection.py
@@ -1,11 +1,12 @@
 import numpy as np
 from math import *
+from tabulate import tabulate
 
 def f(x):
     return (np.log(np.sin(x)**2+1)) - 1/2;
     
 def bisection(f,a,b,tol,n):
-    
+    resultados=[]
     if f(a)*f(b)>=0:
         print("the interval does not change sign");
         
@@ -13,7 +14,6 @@ def bisection(f,a,b,tol,n):
     i = 1;
     while i <= n and e_abs > tol:
         c = (a + b)/2;
-        print("ite",i,"a =",a,"b =",b,"c =",c);
         if f(c)==0:
             print("solution found in x =", c);
             break;
@@ -24,11 +24,18 @@ def bisection(f,a,b,tol,n):
             a = c;
             c_t = b;
         e_abs = abs(c_t - c);
+        if(i!=1):
+            resultados.append([i,a,c,b,f(c),e_abs])
+        else:
+            resultados.append([i,a,c,b,f(c),""])
+
+
         if e_abs < tol:
             print("Solution found in x =", c, ",iteration: ", i);
             break;
         i += 1;
     if i > n:
         print("solution not found for tolerance:" , tol,"spend iterations:", i-1);
+    print(tabulate(resultados, headers=["iter", "a", "xm", "b", "f(xm)", "error"], tablefmt="github", floatfmt=(".0f",".10f",".10f",".10f")))
             
 bisection(f,0,1,10**-7,100);                              
--- methods/python-methods/root.py
+++ methods/python-methods/root.py
@@ -1,5 +1,7 @@
 from math import *
 import numpy as np
+from tabulate import tabulate
+
 
 def f(x):
   return np.exp(x) - x - 1;
@@ -14,18 +16,17 @@ def root(f,df,d2f,x0,tol,n):
   xant = x0;
   fant = f(xant);
   e_abs=1000;
-  iteration = 1;
+  iteration = 0;
+  resultados =[[iteration,xant,f(xant),""]]
   
   while iteration<=n:
     xact = xant - fant * df(xant) / ((df(xant))**2 - fant * d2f(xant));
     fact = f(xact);
     e_abs = abs(xact-xant);
-    
-    print("Iteration:", iteration, "  X =", xact, "F(x) =", fact, "Error =", e_abs);
-    
     iteration += 1;
     xant = xact;
     fant = fact;
+    resultados.append([iteration,xant,f(xant),e_abs])    
     
     if e_abs<tol:
       print("Solution found in X =", xact, "     iterations:", iteration-1, "    error =", e_abs);
@@ -33,5 +34,6 @@ def root(f,df,d2f,x0,tol,n):
   
   if iteration > n:
     print("Solution not found for tolerance = ", tol);
-    
+  print(tabulate(resultados, headers=["iter", "Xi", "f(x)", "error"], tablefmt="github"))
+
 root(f,df,d2f,1,10**-7,100);
