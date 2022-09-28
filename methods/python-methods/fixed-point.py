ded41ed5679d574c233d485f9675497a6b025b17
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-09-28 10:09:03 -0500

    Fixed point corrected

--- methods/python-methods/fixed-point.py
+++ methods/python-methods/fixed-point.py
@@ -1,28 +1,29 @@
 from math import *
 import numpy as np
-
-
+from tabulate import tabulate
 
 def g(x):
-    return (np.log(np.sin(x)**2+1)) - 1/2;           #evaluate the function
+    return (np.log(np.sin(x)**2+1)) - 1/2  # evaluate the function
 
-def punto_fijo(x0, tol, itermax):   
-    iter = 1;
-    while iter<=itermax:            
-        x1 = g(x0);                 #evaluate function in last point 
-        error = abs((x1-x0)/x1);    
-        print("iteration", iter, "point", iter, "function", x1, "error", error); #show table
-        
-        if error < tol:             #if we reach the desired tolerance stop
-            print("solution found in iteration:", iter, "with x=", x1 );
-            break;
-        
-        x0 = x1;                
-        iter += 1;               
-    
+
+def f1(x):
+    return (np.log(np.sin(x)**2+1)) - 1/2 - x
+
+
+def punto_fijo(x0, tol, itermax):
+    iter = 0
+    resultados = [[iter, x0,  g(x0), f1(x0), "NA"]]
+    while iter <= itermax:
+        x1 = g(x0)  # evaluate function in last point
+        error = abs(f1(x0))
+        x0 = x1
+        iter += 1
+        resultados.append([iter,x0,g(x0), f1(x0), error])
+        if error < tol:  # if we reach the desired tolerance stop
+            break
     if iter > itermax:
-        print("solution not found, iterations used: ", iter);  
-    
+        print("solution not found, iterations used: ", iter)
 
-punto_fijo(-0.5, 10**-7, 100);
-        
+    print(tabulate(resultados, headers=["iteration", "Xi", "g(xi)", "f(x)", "error"]))
+
+punto_fijo(-0.5, 10**-7, 100)
