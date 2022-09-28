8b11e22bc8368dec37933c0e38d4614ce424f97e
bened18 <belt@eafit.edu.co>
2022-09-28 11:22:15 -0500

    print added to gauss-simple

--- methods/python-methods/gauss-simple.py
+++ methods/python-methods/gauss-simple.py
@@ -21,10 +21,12 @@ tamano = np.shape(a)
 n = tamano[0]
 print(n)
 
+print("Etapa 0");
 print(M)
 print("")
 
 for i in range(0,n-1,1):
+    print("Etapa ", i+1);
     for j in range(i+1,n,1):
         if M[j,i]!=0:
             M[j,i:n+1]=M[j,i:n+1]-(M[j,i]/M[i,i])*M[i,i:n+1]
@@ -35,6 +37,8 @@ ultfila = n-1;
 ultcolumna = n;
 x = np.zeros(n,dtype=float);
 i = ultfila;
+print("Despues de aplicar sustitucion regresiva");
+print("x:");
 for i in range(ultfila,0-1,-1):
     suma = 0;
     for j in range(i+1,ultcolumna,1):
