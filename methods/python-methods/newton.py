7db4d9758fedfe1fce995bc14512d7b36a21f9c5
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-09-28 11:07:18 -0500

    added table to newton

--- methods/python-methods/newton.py
+++ methods/python-methods/newton.py
@@ -1,6 +1,8 @@
 from re import A
 import numpy as np
 from math import *
+from tabulate import tabulate
+
 
 def f(x):
     return (np.log(np.sin(x)**2+1)) - 1/2;
@@ -9,6 +11,7 @@ def df(x):
     
 def newton(f, df, p_0, tol, n):
     print("iteration: ", 0, "p = ", p_0);
+    resultados=[[0, p_0, f(p_0),""]]
     e_abs = 1;
     i = 1;
     while i <= n:
@@ -18,8 +21,7 @@ def newton(f, df, p_0, tol, n):
             
         p_1 = p_0 - (f(p_0))/(df(p_0)); #method formula
         e_abs = abs(p_1 - p_0);
-        print("iteration: ", i, "p = ", p_1, "error = ", e_abs);
-        
+        resultados.append([i,p_1,f(p_1),e_abs])
         if e_abs < tol: #stop criterion
             print("solution found in x = ", p_1, "iterations: ", i);
             break;
@@ -27,5 +29,7 @@ def newton(f, df, p_0, tol, n):
         i += 1;
     if i > n:
         print("solution not found for tolerance:" , tol,"spend iterations:", i-1);
+    print(tabulate(resultados, headers=["iter", "Xi", "f(xi)", "error"], tablefmt="github", floatfmt=(".10f",".10f")))
+
 
 newton(f,df,0.5,10**-7,100);
