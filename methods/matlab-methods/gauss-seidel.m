8c0117d021d8c263ecae59474389c33a41bd37e1
bened18 <belt@eafit.edu.co>
2022-11-01 18:38:21 -0500

    matlab methods added

---
+++ methods/matlab-methods/gauss-seidel.m
@@ -0,0 +1,43 @@
+
+%This program finds the solution to the system Ax=b using the method of
+%Gauss–Seidel
+
+%Tickets:
+%A, invertible matrix
+%b, constant vector
+%x0, initial approximation
+%tol, tolerance
+%Nmax, maximum number of iterations
+
+%Departures
+%x, solution
+%iter, number of iterations
+%err, error
+
+
+
+function [x,iter,err]=C17_gseidel(A,b,x0,tol,Nmax)
+
+    %Inicialización 
+    D=diag(diag(A));
+    L=-tril(A)+D;
+    U=-triu(A)+D;
+    T=inv(D-L)*U; 
+    C=inv(D-L)*b;
+    xant=x0;
+    E=1000;
+    cont=0;
+    
+    %Ciclo
+    while E>tol && cont<Nmax       
+        xact=T*xant+C;
+        E=norm(xant-xact);
+        xant=xact;
+        cont=cont+1;
+    end
+    
+    %Entrega de resultados
+    x=xact;
+    iter=cont;
+    err=E;
+    end
