23f66034b3735605cd7b046a67e8b52803d9f824
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-11-17 17:27:52 -0500

    lineal spline view

--- apps/interpolations/views.py
+++ apps/interpolations/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 
 from apps.interpolations.functions.CubicSpline import traza3natural
 from apps.interpolations.functions.Lagrange import lagrange
+from apps.interpolations.functions.LinealSpline import lineal_spline
 
 
 class CubicSplineTemplateView(TemplateView):
@@ -35,7 +36,18 @@ class LagrangeTemplateView(TemplateView):
 
 class LinealSplineTemplateView(TemplateView):
     template_name = "interpolations/LinealSpline.html"
+    def get_context_data(self, **kwargs):
+        context = super(CubicSplineTemplateView,
+                        self).get_context_data(**kwargs)
 
+        xi = self.request.GET.get('xi', '')
+        fi = self.request.GET.get('fi', '')
+
+        if xi and fi:            
+            context['polynomial_result'] = lineal_spline(xi, fi)[0]
+            context['section_result'] = lineal_spline(xi, fi)[1]
+
+        return context
 
 class NewtonInterpolationTemplateView(TemplateView):
     template_name = "interpolations/NewtonInterpolation.html"
