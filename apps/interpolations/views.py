80dbaf73eed020518c91f22548f993482851e6fc
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-11-17 11:31:45 -0500

    starting with helps

--- apps/interpolations/views.py
+++ apps/interpolations/views.py
@@ -5,20 +5,14 @@ from apps.interpolations.functions.CubicSpline import inputcubic
 
 class CubicSplineTemplateView(TemplateView):
     template_name = "interpolations/CubicSpline.html"
-
     def get_context_data(self, **kwargs):
         context = super(CubicSplineTemplateView,
                         self).get_context_data(**kwargs)
-
         inputx1 = self.request.GET.get('xi', '')
         inputf1 = self.request.GET.get('fi', '')
-
         if inputx1 and inputf1:
-
             functionresult = inputcubic(inputx1, inputf1)
-
             context['result'] = f"{functionresult}"
-
         return context
 
 
