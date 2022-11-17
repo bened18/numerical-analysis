8d96836771a7dd7866e2782e782d69b61ff910cb
Sebastian Morales <sebastian@sebasmd.com>
2022-11-17 15:28:21 -0500

    Bisection table bug fix

--- app_core_numericalanalysis/settings.py
+++ app_core_numericalanalysis/settings.py
@@ -17,34 +17,20 @@ SECRET_KEY = "django-4.1.3-)-&q5g+4wrp=w3q$$!yd_c(6x$=ls52^h+!1b#vsvp1h^#dql7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Debug mode
-if environment == 'prod':
-    DEBUG = False
-else:
+if environment == "local":
     DEBUG = True
-
-if environment != 'prod':
-    CORS_REPLACE_HTTPS_REFERER = False
-    HOST_SCHEME = "http://"
-    SECURE_PROXY_SSL_HEADER = None
-    SECURE_SSL_REDIRECT = False
-    SESSION_COOKIE_SECURE = False
-    CSRF_COOKIE_SECURE = False
-    SECURE_HSTS_SECONDS = None
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-    SECURE_FRAME_DENY = False
-
-# SSL Redirect
-if environment == 'prod' or environment == 'dev':
-    SECURE_SSL_REDIRECT = True
+    CORS_REPLACE_HTTPS_REFERER      = False
+    HOST_SCHEME                     = "http://"
+    SECURE_PROXY_SSL_HEADER         = None
+    SECURE_SSL_REDIRECT             = False
+    SESSION_COOKIE_SECURE           = False
+    CSRF_COOKIE_SECURE              = False
+    SECURE_HSTS_SECONDS             = None
+    SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+    SECURE_FRAME_DENY               = False
+    ALLOWED_HOSTS = ["192.168.1.25", "127.0.0.1", "localhost"]
 else:
-    SECURE_SSL_REDIRECT = False
-
-# Allowed hosts
-if environment != 'prod':
-    ALLOWED_HOSTS = ["*", "192.168.1.25", "127.0.0.1", "localhost"]
-else:
-    ALLOWED_HOSTS = ["bened18.pythonanywhere.com",
-                     "Bened18.pythonanywhere.com"]
+    ALLOWED_HOSTS = ["bened18.pythonanywhere.com"]
 
 # Application definition
 # Django Apps
