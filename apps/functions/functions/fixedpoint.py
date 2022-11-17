4462f09c82449c802f0c1a6769f17bce9e4a6865
bened18 <belt@eafit.edu.co>
2022-11-16 21:34:08 -0500

    library math imported

--- apps/functions/functions/fixedpoint.py
+++ apps/functions/functions/fixedpoint.py
@@ -2,30 +2,28 @@ from math import *
 import numpy as np
 from tabulate import tabulate
 
+def g(g1, x):
+    return eval(g1)  # evaluate the function
 
-def g(g, x):
-    return eval(g)
 
-
-def f1(f1, x):
+def f(f1, x):
     return eval(f1)
 
 
-def fixed_point(g, f1, x0, tol, itermax):
+def punto_fijo(g1, f1, x0, tol, itermax):
     iter = 0
-    error = ''
-    resultados = [[iter, x0,  g(g, x0), f1(f1, x0), error]]
+    resultados = [[iter, x0,  g(g1, x0), f(f1, x0), "NA"]]
     while iter <= itermax:
-        x1 = g(g, x0)  # evaluate function in last point
-        error = abs(f1(f1, x0))
+        x1 = g(g1, x0)  # evaluate function in last point
+        error = abs(f(f1, x0))
         x0 = x1
         iter += 1
-        resultados.append([iter, x0, g(g, x0), f1(f1, x0), error])
+        resultados.append([iter,x0,g(g1, x0), f(f1, x0), error])
         if error < tol:  # if we reach the desired tolerance stop
             break
     if iter > itermax:
-        return f"solution not found, iterations used: {iter}"
+        return("solution not found, iterations used: ", iter)
 
-    return (tabulate(resultados, headers=["iteration", "Xi", "g(xi)", "f(x)", "error"], tablefmt="html", floatfmt=(".10f", ".10f", ".10f")))
+    return(tabulate(resultados, headers=["iteration", "Xi", "g(xi)", "f(x)", "error"], tablefmt="html", floatfmt=(".10f",".10f",".10f")))
 
-# punto_fijo(-0.5, 10**-7, 100)
+#punto_fijo("log(pow(sin(x),2) + 1) - (1/2)", "log(pow(sin(x),2) + 1) - (1/2)", -0.5, 10**-7, 100)
