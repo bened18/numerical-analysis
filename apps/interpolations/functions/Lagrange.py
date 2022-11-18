7a224bc89c408f16c1d163bcdb0513fe5d8d85fa
Sebastian Morales <sebastian@sebasmd.com>
2022-11-17 21:26:25 -0500

    Lagrange html with chart

--- apps/interpolations/functions/Lagrange.py
+++ apps/interpolations/functions/Lagrange.py
@@ -1,21 +1,38 @@
 import numpy as np
 import sympy as sym
+import json
+from math import *
 import matplotlib.pyplot as plt
 
-def lagrange():
-    xi = np.array([-1,0,3,4])
-    fi = np.array([15.5,3,8,1])
+
+def convert_string_to_list(string):
+    """
+        Receive a comma-separated string and convert it to its type
+        exm:
+            convert_string_to_list("1,2.3,4,5.6")
+        result:
+            [1,2.3,4,5.6]
+            int,float,int,float
+    """
+    res = f"[{string}]".strip(" ")
+    res_to_json = json.loads(res)
+    return res_to_json
+
+
+def lagrange(xi_str, fi_str):
+    xi = convert_string_to_list(xi_str)
+    fi = convert_string_to_list(fi_str)
 
     n = len(xi)
     x = sym.Symbol('x')
     polinomio = 0
-    divisorL = np.zeros(n, dtype = float)
-    for i in range(0,n,1):
-        
+    divisorL = np.zeros(n, dtype=float)
+    for i in range(0, n, 1):
+
         numerador = 1
         denominador = 1
-        for j  in range(0,n,1):
-            if (j!=i):
+        for j in range(0, n, 1):
+            if (j != i):
                 numerador = numerador*(x-xi[j])
                 denominador = denominador*(xi[i]-xi[j])
         terminoLi = numerador/denominador
@@ -23,24 +40,34 @@ def lagrange():
         polinomio = polinomio + terminoLi*fi[i]
         divisorL[i] = denominador
 
-    polisimple = polinomio.expand()
+    polisimple = sym.expand(polinomio)
 
-    px = sym.lambdify(x,polisimple)
+    px = sym.lambdify(x, polisimple)
 
     muestras = 101
-    a = np.min(xi)
-    b = np.max(xi)
-    pxi = np.linspace(a,b,muestras)
+    a = min(xi, default=0)
+    b = max(xi, default=0)
+    pxi = np.linspace(a, b, muestras)
     pfi = px(pxi)
 
-    print('    fi values: ',fi)
-    print('dividers L(i): ',divisorL)
-    print()
-    print('Lagrange polynomial, expressions')
-    print(polinomio)
-    print()
-    print('Lagrange polynomial ')
-    print(polisimple)
+    return {
+        "fi": fi,
+        "dividers": divisorL,
+        "lpe": polinomio,
+        "lp": polisimple,
+        "xi": xi,
+        "pxi": pxi,
+        "pfi": pfi
+    }
+
+    # print('    fi values: ',fi)
+    # print('dividers L(i): ',divisorL)
+    # print()
+    # print('Lagrange polynomial, expressions')
+    # print(polinomio)
+    # print()
+    # print('Lagrange polynomial ')
+    # print(polisimple)
 
     # plt.plot(xi,fi,'o', label = 'Point')
     # plt.plot(pxi, pfi, label = 'Polynomial')
@@ -49,3 +76,5 @@ def lagrange():
     # plt.ylabel('fi')
     # plt.title('Lagrange interpolation')
     # plt.show()
+
+# print(lagrange("-1,0,3,4", "15.5,3,8,1"))
--- apps/interpolations/views.py
+++ apps/interpolations/views.py
@@ -27,8 +27,17 @@ class LagrangeTemplateView(TemplateView):
                         self).get_context_data(**kwargs)
         xi = self.request.GET.get('xi', '')
         fi = self.request.GET.get('fi', '')
+        
+        l_dict = lagrange(xi, fi)
+        
         if xi and fi:
-            context["activate"] = lagrange()
+            context["fi"] = l_dict["fi"]
+            context["dividers"] = l_dict["dividers"]
+            context["lagrangePolynomialExpresion"] = l_dict["lpe"]
+            context["lagrangePolynomial"] = l_dict["lp"]
+            context["xi"] = l_dict["xi"]
+            context["pxi"] = l_dict["pxi"]
+            context["pfi"] = l_dict["pfi"]
         
         return context
 
