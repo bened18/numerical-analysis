9e0dffbd38b46f610625ba0b7b657f0cbcf6a827
Banjamin <belt@eafit.edu.co>
2022-11-22 13:07:12 -0500

    plot f in fixed point

--- public/static/assets/js/plotter.js
+++ public/static/assets/js/plotter.js
@@ -14,6 +14,22 @@ let parameters = {
 };
 
 
+let parameters2 = {
+    target: '#myFunction2',
+    data: [{
+        fn: 'sin(x)',
+        color: 'red'
+    }],
+    grid: true,
+    yAxis: {
+        domain: [-1, 1]
+    },
+    xAxis: {
+        domain: [0, 2 * Math.PI]
+    }
+};
+
+
 function plot() {
     var f = document.querySelector("#function").value;
     var xMin = document.querySelector("#xMin").value;
@@ -29,3 +45,20 @@ function plot() {
 
     functionPlot(parameters);
 }
+
+function plotg() {
+    var g = document.querySelector("#function2").value;
+    var xMin2 = document.querySelector("#xMin2").value;
+    var xMax2 = document.querySelector("#xMax2").value;
+    var yMin2 = document.querySelector("#yMin2").value;
+    var yMax2 = document.querySelector("#yMax2").value;
+    var color2 = document.querySelector("#color2").value;
+
+    parameters2.data[0].fn = g;
+    parameters2.xAxis.domain = [xMin2, xMax2];
+    parameters2.yAxis.domain = [yMin2, yMax2];
+    parameters2.data[0].color = color2;
+
+    functionPlot(parameters2);
+}
+
